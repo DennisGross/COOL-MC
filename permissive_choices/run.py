58881ebf00d15dd1f8920bba493d0a06cb0df596
Dennis Gross <dgross@science.ru.nl>
2021-11-11 14:31:51 +0100

    More description

--- permissive_choices/run.py
+++ permissive_choices/run.py
@@ -77,6 +77,7 @@ if __name__ == '__main__':
     # Generate actions
     m_permissive_manager = PermissiveManager(command_line_arguments['permissive_input'], env.storm_bridge.state_mapper)
     all_pairs = m_permissive_manager.manage_permissive_state_actions_pairs(state, m_project.agent)
+    print("All decisions for state", state,":")
     for key in all_pairs.keys():
         print(key, env.action_mapper.action_index_to_action_name(all_pairs[key]))
 
