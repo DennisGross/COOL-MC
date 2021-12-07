e5f4e075daab5d7c3e78eb8fed6b615818297ef5
Dennis Gross <dgross@science.ru.nl>
2021-12-07 15:00:39 +0100

    Added Abstraction Mapping

--- common/safe_gym/abstraction_manager.py
+++ common/safe_gym/abstraction_manager.py
@@ -1,26 +1,34 @@
 import numpy as np
 import math
-
+import os
+import json
 class AStateVariable:
 
-    def __init__(self, name, lower_bound, upper_bound, step_size):
+    def __init__(self, name, lower_bound, upper_bound, step_size, mapping=None):
         self.name = name
         self.lower_bound = lower_bound
         self.upper_bound = upper_bound
         self.step_size = step_size
         self.all_assignments = range(self.lower_bound,self.upper_bound+self.step_size,self.step_size)
+        self.mapping = mapping
         
 
     def round(self, value):
-        min_distance = math.inf
-        min_idx = 0
-        
-        for idx in range(len(self.all_assignments)):
-            distance = abs(value - self.all_assignments[idx])
-            if distance < min_distance:
-                min_idx = idx
-                min_distance = distance
-        return self.all_assignments[min_idx]
+        if self.mapping is None:
+            min_distance = math.inf
+            min_idx = 0
+            for idx in range(len(self.all_assignments)):
+                distance = abs(value - self.all_assignments[idx])
+                if distance < min_distance:
+                    min_idx = idx
+                    min_distance = distance
+            return self.all_assignments[min_idx]
+        else:
+            for original_values in self.mapping.keys():
+                if str(original_values).find(str(int(value))) != -1:
+                    return self.mapping[original_values]
+            raise ValueError("Mapping for " + str(value) + " does not exist.")
+
 
     def set_idx(self, idx):
         self.idx = idx
@@ -54,15 +62,29 @@ class AStateVariable:
             
         return astate_variables
 
+    @staticmethod
+    def parse_abstraction_from_dict(abstraction_mapping):
+        astate_variables = []
+        for state_variable_name in abstraction_mapping.keys():
+            astate_variable = AStateVariable(state_variable_name, -1, -1, -1, mapping=  abstraction_mapping[state_variable_name])
+            astate_variables.append(astate_variable)
+        return astate_variables
+
 
 class AbstractionManager:
 
 
     def __init__(self, state_var_mapper, abstraction_input) -> None:
         self.is_active = (abstraction_input != '')
+        self.state_var_mapper = state_var_mapper.openai_state_variable_positions
         if self.is_active:
-            self.state_var_mapper = state_var_mapper.openai_state_variable_positions
-            self.astate_variables = AStateVariable.parse_abstraction_variables(abstraction_input)
+            if os.path.isfile(abstraction_input):
+                with open(abstraction_input) as json_file:
+                    abstraction_mapping = json.load(json_file)
+                    self.astate_variables = AStateVariable.parse_abstraction_from_dict(abstraction_mapping)
+            else:
+                self.astate_variables = AStateVariable.parse_abstraction_variables(abstraction_input)
+            
             for i in range(len(self.astate_variables)):
                 self.astate_variables[i].set_idx(self.state_var_mapper[self.astate_variables[i].name])
 
